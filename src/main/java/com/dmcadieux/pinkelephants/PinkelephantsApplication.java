b37920ce34c961aa0dd3baa4b1721b3d74c5cce0
dmcadieux <dmcadieux@gmail.com>
2024-08-21 22:46:55 -0400

    first commit

---
+++ src/main/java/com/dmcadieux/pinkelephants/PinkelephantsApplication.java
@@ -0,0 +1,13 @@
+package com.dmcadieux.pinkelephants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkelephantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkelephantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmcadieux/pinkelephants/PinkelephantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmcadieux.pinkelephants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkelephantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
